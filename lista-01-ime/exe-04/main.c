ddf2de9e7fd7791e9ef679608d805e3405419cd3
Blabos de Blebe <blabos@blabos.org>
2009-09-13 15:21:55 -0300

    Adicionando resolução do exercício 4 da primeira lista

---
+++ lista-01-ime/exe-04/main.c
@@ -0,0 +1,20 @@
+/*
+ * main.c
+ *
+ *  Created on: Sep 13, 2009
+ *      Author: wesley
+ *  
+ *  4. Durante os 30 dias do mês de Fevereiro foram tomadas as temperaturas
+ *  médias diárias de Euclides da Cunha (BA)(1). Determinar o número de dias
+ *  desse mês com temperaturas abaixo de zero
+ *  
+ *  R. Fevereiro não tem 30 dias e eu duvido que exista algum lugar na Bahia
+ *  que tenha tido temperatura abaixo de zero desde a última era glacial.
+ *  Indo para um exercício com enunciado melhor :)
+ */
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	return EXIT_FAILURE;
+}
