e7477e20085439125add410f94be6e9a690d19be
Blabos de Blebe <blabos@blabos.org>
2009-09-16 02:42:54 -0300

    Adicionei a resolução do exercício 11 da lista 1

---
+++ lista-01-ime/exe-11/main.c
@@ -0,0 +1,44 @@
+/*
+ * main.c
+ *
+ *  Created on: Sep 16, 2009
+ *      Author: blabos
+ *  
+ *  11. Dizemos que um número natural é triangular se ele é produto de três
+ *  números naturais consecutivos.
+ *  
+ *  Exemplo: 120 é triangular, pois 4.5.6 = 120. Dado n natural, verificar se
+ *  n é triangular.
+ */
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX_BUFF_SIZE 16
+
+int is_triangular(int n) {
+	int i = 2;
+	int p = 0;
+	while ((p = (i-1)*(i)*(i+1)) <= n) {
+		if (p == n) return 1;
+		i++;
+	}
+	return 0;
+}
+
+int main(void) {
+	char buffer[MAX_BUFF_SIZE];
+	int n, i, j, k, count;
+	
+	printf("Enter n: ");
+	fgets(buffer, MAX_BUFF_SIZE, stdin);
+	n = buffer != NULL ? atoi(buffer) : 0;
+	
+	if (is_triangular(n)) {
+		printf("%d is triangular\n", n);
+	}
+	else {
+		printf("%d isn't triangular\n", n);
+	}
+	
+	return EXIT_SUCCESS;
+}
