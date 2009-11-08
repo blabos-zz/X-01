7f6bf4102f96dc16a03050d047fe4f045208c356
Blabos de Blebe <blabos@blabos.org>
2009-11-08 03:46:18 -0200

    Trocando tabs por espaços

--- exercises/sentinels/list.h
+++ exercises/sentinels/list.h
@@ -9,28 +9,28 @@
 #define LIST_H_
 
 struct node_st {
-	int				data;
-	struct node_st*	next;
-	struct node_st*	prev;
+    int             data;
+    struct node_st* next;
+    struct node_st* prev;
 };
 
 typedef struct node_st node_t;
 
 typedef struct {
-	node_t*			nil;
+    node_t*         nil;
 } list_t;
 
 
-list_t*		new_list();
-void		del_list(list_t*);
-void		print_list(list_t*);
+list_t*     new_list();
+void        del_list(list_t*);
+void        print_list(list_t*);
 
-node_t*		head(list_t*);
-node_t*		tail(list_t*);
+node_t*     head(list_t*);
+node_t*     tail(list_t*);
 
-node_t*		find(list_t*, int);
-node_t*		insert(list_t*, int);
-void		delete(list_t*, node_t*);
+node_t*     find(list_t*, int);
+node_t*     insert(list_t*, int);
+void        delete(list_t*, node_t*);
 
 
 #endif /* LIST_H_ */
--- exercises/sentinels/main.c
+++ exercises/sentinels/main.c
@@ -10,21 +10,21 @@
 #include <stdio.h>
 
 int main(void) {
-	list_t* list = new_list();
-	node_t* node;
-	
-	insert(list, 1);
-	insert(list, 2);
-	insert(list, 3);
-	
-	print_list(list);
-	
-	node = find(list, 2);
-	delete(list, node);
-	
-	printf("\n");
-	print_list(list);
-	
-	del_list(list);
-	return EXIT_SUCCESS;
+    list_t* list = new_list();
+    node_t* node;
+    
+    insert(list, 1);
+    insert(list, 2);
+    insert(list, 3);
+    
+    print_list(list);
+    
+    node = find(list, 2);
+    delete(list, node);
+    
+    printf("\n");
+    print_list(list);
+    
+    del_list(list);
+    return EXIT_SUCCESS;
 }
