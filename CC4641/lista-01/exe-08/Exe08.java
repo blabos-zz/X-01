30704ae4f59ca90a503e81a46c7224f529c655df
Blabos de Blebe <blabos@blabos.org>
2010-03-05 01:57:21 -0300

    Exercício 0108 - Adendos

--- CC4641/lista-01/exe-08/Exe08.java
+++ CC4641/lista-01/exe-08/Exe08.java
@@ -1,70 +1,107 @@
+import org.blabos.lista1.Employer;
+import java.io.*;
+
 public class Exe08 {
-    public static void main(String args[]) {
-        Employer e = new Employer();
+    private Employer[] employers;
+    
+    public static void main(String args[]) throws Exception {
+        Exe08 exe = new Exe08();
         
-        System.out.println("Nome:            " + e.name() );
-        System.out.println("Salario:         " + e.salary() );
-        System.out.println("Horas Extras:    " + e.all_overtimes() );
-        System.out.println("Salario Familia: " + e.family_salary() );
-        System.out.println("Salario Bruto:   " + e.brute_salary() );
-        System.out.println("Descontos:       " + e.all_discounts() );
-        System.out.println("Salario Liquido: " + e.brute_salary() );
+        
+        exe.init();
+        exe.show();
+    }
+    
+    private void init() {
+        BufferedReader stdin
+            = new BufferedReader(new InputStreamReader(System.in));
+        
+        String input;
+        int nfunc;
+        
+        System.out.print("Digite a quantidade de funcionarios: ");
+        try {
+            input = stdin.readLine();
+            nfunc = Integer.parseInt(input);
+        }
+        catch (Exception e) {
+            System.err.println("Warning: n func invalido. Usando 3");
+            nfunc = 3;
+        }
+        
+        employers = new Employer[nfunc];
+        
+        String name             = "";
+        double salary           = 0.0;
+        double day_overtimes    = 0.0;
+        double night_overtimes  = 0.0;
+        double discounts        = 0.0;
+        double meals            = 0.0;
+        double vales            = 0.0;
+        int dependents          = 0;
+        int absences            = 0;
+        
+        for (int i = 0; i < employers.length; i++) {
+            try {
+                System.out.print("Nome:              ");
+                name = stdin.readLine();
+                
+                System.out.print("Salario:           ");
+                salary = Double.parseDouble(stdin.readLine());
+                
+                System.out.print("Horas Ex Diurnas:  ");
+                day_overtimes = Double.parseDouble(stdin.readLine());
+                
+                System.out.print("Horas Ex Noturnas: ");
+                night_overtimes = Double.parseDouble(stdin.readLine());
+                
+                System.out.print("Dependentes:       ");
+                dependents = Integer.parseInt(stdin.readLine());
+                
+                System.out.print("Faltas:            ");
+                dependents = Integer.parseInt(stdin.readLine());
+                
+                System.out.print("Descontos:         ");
+                discounts = Double.parseDouble(stdin.readLine());
+                
+                System.out.print("Comida:            ");
+                meals = Double.parseDouble(stdin.readLine());
+                
+                System.out.print("Vales:             ");
+                vales = Double.parseDouble(stdin.readLine());
+            }
+            catch (Exception e) {
+                System.err.println("Voce digitou besteira, se lascou!!!");
+                System.exit(-1);
+            }
+                
+            employers[i] = new Employer(
+                name,
+                salary,
+                day_overtimes,
+                night_overtimes,
+                discounts,
+                meals,
+                vales,
+                dependents,
+                absences
+            );
+        }
+    }
+    
+    private void show() {
+        System.out.println("\nLista de funcionarios:");
+        for (int i = 0; i < employers.length; i++) {
+            System.out.println("\nFuncionário " + i + ":" );
+            System.out.println("Nome:            " + employers[i].name() );
+            System.out.println("Salario:         " + employers[i].salary() );
+            System.out.println("Horas Extras:    " + employers[i].all_overtimes() );
+            System.out.println("Salario Familia: " + employers[i].family_salary() );
+            System.out.println("Salario Bruto:   " + employers[i].brute_salary() );
+            System.out.println("Descontos:       " + employers[i].all_discounts() );
+            System.out.println("Salario Liquido: " + employers[i].brute_salary() );
+        }
     }
 };
 
-class Employer {
-    String      name;
-    double      salary;
-    double      day_overtimes;
-    double      nigth_overtimes;
-    int         dependents;
-    int         absences;
-    double      eventual_discounts;
-    double      meals;
-    double      vales; //???
-    
-    public Employer() {
-        name                = "Blabos de Blebe";
-        salary              = 10000.0;
-        day_overtimes       = 20;
-        nigth_overtimes     = 10;
-        dependents          = 9;
-        absences            = 10;
-        eventual_discounts  = 30;
-        meals               = 200;
-        vales               = 300;
-    }
-    
-    public String name() {
-        return name;
-    }
-    
-    public double salary() {
-        return salary;
-    }
-    
-    public double received_salary() {
-        return brute_salary() - all_discounts();
-    }
-    
-    public double all_overtimes() {
-        return salary * (day_overtimes + nigth_overtimes * 1.2) / 160;
-    }
-    
-    public double family_salary() {
-        return dependents * 0.05 * 550;
-    }
-    
-    public double brute_salary() {
-        return salary + all_overtimes() + family_salary();
-    }
-    
-    public double all_discounts() {
-        return (
-            0.8 * salary +                          // INAMPS
-            salary * absences / 160 +               // Faltas
-            meals + vales + eventual_discounts +    // Comida, vales e outros
-            0.08 * brute_salary()                   // IR
-        );
-    }
-};
+
