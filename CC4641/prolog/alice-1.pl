61d1c5b6f316701f5f11be24a1d85fc224f6519b
Blabos de Blebe <blabos@blabos.org>
2010-03-26 01:08:45 -0300

    Exercício de prolog (alice) comentado

---
+++ CC4641/prolog/alice-1.pl
@@ -0,0 +1,73 @@
+mentira(leao, segunda).
+mentira(leao, terca).
+mentira(leao, quarta).
+
+mentira(unicornio, quinta).
+mentira(unicornio, sexta).
+mentira(unicornio, sabado).
+
+verdade(leao, quinta).
+verdade(leao, sexta).
+verdade(leao, sabado).
+verdade(leao, domingo).
+
+verdade(unicornio, domingo).
+verdade(unicornio, segunda).
+verdade(unicornio, terca).
+verdade(unicornio, quarta).
+
+ontem(segunda, domingo).
+ontem(terca, segunda).
+ontem(quarta, terca).
+ontem(quinta, quarta).
+ontem(sexta, quinta).
+ontem(sabado, sexta).
+ontem(domingo, sabado).
+
+% Para valer esta afirmação, no dia em que ela é dita o personagem precisa
+% estar falando a verdade e ainda ter mentido no dia anterior.
+menti_ontem(X, Y) :- verdade(X, Y), ontem(Y, Z), mentira(X, Z).
+
+% Ou ambos falam a verdade e mentiram ontem.
+hoje(X) :- verdade(leao,X), menti_ontem(leao, X), verdade(unicornio,X),
+    menti_ontem(unicornio, X).
+
+% Ou somente o unicórnio mente e portanto sua afirmação é falsa.
+hoje(X) :- verdade(leao,X), menti_ontem(leao, X), mentira(unicornio,X),
+    not(menti_ontem(unicornio, X)).
+
+% Ou somente o leão mente e portanto sua afirmação é falsa.
+hoje(X) :- mentira(leao,X), not(menti_ontem(leao, X)), verdade(unicornio,X),
+    menti_ontem(unicornio, X).
+
+% Ou ambos mentem e portanto as duas afirmações são falsas.
+hoje(X) :- mentira(leao,X), not(menti_ontem(leao, X)), mentira(unicornio,X),
+    not(menti_ontem(unicornio, X)).
+
+% Analisando as quatro equações finais:
+%
+% 1) Se ambos falam a verdade e dizem no mesmo dia que mentiram ontem, há uma
+% contradição, pois, o leão só pode falar a verdade ao afirmar que mentiu no
+% dia anterior quando for uma quinta. Já o unicórnio só falará a verdade ao
+% afirmar que mentiu no dia anterior quando for um domingo. Portanto ambos
+% jamais podem estar falando a verdade simultaneamente quando afirmam que
+% mentiram no dia anterior.
+%
+% 2) Se o leão diz a verdade, ele só pode afirmar que mentiu no dia anterior
+% se hoje for uma quinta. Da mesma forma, o unicórnio só mente ao afirmar que
+% mentiu no dia anterior, quando for uma quinta. Então quinta é um dia
+% dedutível.
+%
+% 3) Se o unicórnio diz a verdade, ele só pode afirmar que mentiu no dia
+% anterior se hoje for um domingo. Já o leão, para mentir afirmando que mentiu
+% no dia anterior, precisa fazer isso numa segunda, pois ele não mente no
+% domingo e estaria falando a verdade se fosse terça ou quarta, dias nos quais
+% sabemos que ele mente, o que gera mais uma contradição.
+%
+% 4) Se ambos mentem, há mais outra contradição pois, enquanto o leão só pode
+% mentir ao afirmar que mentiu no dia anterior se hoje for uma segunda, o
+% unicório só mente ao afirmar isso quando hoje for quinta. Portanto ambos
+% não podem estar simultaneamente mentindo ao afirmar que mentiram no dia
+% anterior.
+%
+% Portanto hoje é quinta feira.
