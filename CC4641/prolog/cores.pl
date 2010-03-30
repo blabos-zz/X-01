672d97ccb653a6fc2cec22080d37716a2fad7622
Mariana <maricapp@gmail.com>
2010-03-30 11:56:44 -0300

    adicionei uma parte do ex cores

---
+++ CC4641/prolog/cores.pl
@@ -0,0 +1,17 @@
+pessoa(alexandra).
+pessoa(barbara).
+pessoa(rosa).
+
+cor(azul).
+cor(verde).
+cor(lilas).
+cor(vermelho).
+cor(amarelo).
+
+veste(alexandra, azul).
+veste(barbara, amarelo).
+veste(rosa, verde).
+
+naoveste(barbara, verde).
+naoveste(rosa, azul).
+naoveste(rosa, lilas).
