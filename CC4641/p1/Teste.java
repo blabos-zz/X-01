a0d5f6aeee4f3e3ff0dc37843f099d3994f7d2ed
Blabos de Blebe <blabos@blabos.org>
2010-04-09 01:15:55 -0300

    Exercício da p1

---
+++ CC4641/p1/Teste.java
@@ -0,0 +1,37 @@
+public class Teste {
+    public static void main(String args[]) {
+        Mae base;
+        Mae m = new Mae();
+        Filha f = new Filha();
+        
+        base = m;
+        base.m1();
+        base.m2();
+        
+        base = f;
+        base.m1();
+        base.m2();
+        
+        f.m2();
+    }
+};
+
+class Mae {
+    public void m1() {
+        System.out.println("Mae.m1()");
+    }
+    
+    public static void m2() {
+        System.out.println("Mae.m2()");
+    }
+};
+
+class Filha extends Mae {
+    public void m1() {
+        System.out.println("Filha.m1()");
+    }
+    
+    public static void m2() {
+        System.out.println("Filha.m2()");
+    }
+};
